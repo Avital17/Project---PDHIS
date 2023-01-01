ebf77292ce4c3c0b3d0b6baa73d245ed09c91d82
nimasli <118096180+nimasli@users.noreply.github.com>
2023-01-01 18:13:06 +0200

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,11 @@
+from django import forms 
+
+
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea())
+
+
